d74efe910cffb2c1de1ac9914db798b675068e7b
Russell McBain <Russell@MacBook-Pro-2.local>
2016-09-12 13:03:20 -0500

    Unit 9

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,3 +3,33 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+if (cardTwo === cardFour) {
+	alert('You found a match!');
+} else {
+  alert('Sorry, try again.');
+}
+if (cardOne === cardTwo) {
+	alert('You found a match!');
+} else {
+  alert('Sorry, try again.');
+}
+if (cardOne === cardThree) {
+	alert('You found a match!');
+} else {
+  alert('Sorry, try again.');
+}
+if (cardOne === cardFour) {
+	alert('You found a match!');
+} else {
+  alert('Sorry, try again.');
+}
+if (cardTwo === cardThree) {
+	alert('You found a match!');
+} else {
+  alert('Sorry, try again.');
+}
+if (cardThree === cardFour) {
+	alert('You found a match!');
+} else {
+  alert('Sorry, try again.');
+}
